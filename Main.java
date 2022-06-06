65da2148b770125040f9d2d5394a89c1f8d24d57
lavamonstr <106985573+ainsleychis@users.noreply.github.com>
2022-06-06 16:08:29 +0000

    added

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("It's laura.");
+    System.out.println("Nice to see you, Laura and Nora!");
   }
 }
